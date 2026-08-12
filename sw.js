4172a7ed0cdfb093b37bece527fd26130308c31d
ringhio324 <81621944+ringhio324@users.noreply.github.com>
2026-08-12 18:43:35 +0900

    Add local storage and backup guidance

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // アプリ本体だけをキャッシュする。写真と音声はIndexedDBにあり、ここには載らない。
-const CACHE = 'talking-album-v11';
-const ASSETS = ['./', './index.html', './app.js?v=11', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
+const CACHE = 'talking-album-v12';
+const ASSETS = ['./', './index.html', './app.js?v=12', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
